df30cfed91a1aa9398ae5d606477e42a61427eb9
Cory Bethune <corybbethune@gmail.com>
2022-08-23 19:14:24 -0700

    Refactor

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/application_cable/"
+  add_filter "/jobs/"
+  add_filter "/mailers/"
+end
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
